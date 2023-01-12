4d1ace999b1d40e98177cc8dac03187b8feff82a
dano <nycosborne@gmail.com>
2023-01-12 15:38:36 +0300

    removed card boarder

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -4,8 +4,8 @@ import Card from 'react-bootstrap/Card';
 function ProjectCard({imageTag, link}) {
 
     return (
-        <Card style={{width: '18rem'}}>
-            <a href={link}><Card.Img variant="top" src={imageTag}/></a>
+        <Card style={{width: '18rem',  background:"lightgray"}}>
+            <a style={{ background:"lightgray"}} href={link}><Card.Img variant="top" src={imageTag}/></a>
         </Card>)
 }
 
